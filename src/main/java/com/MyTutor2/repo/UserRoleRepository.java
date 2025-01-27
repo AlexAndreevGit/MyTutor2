1decb54536a1b61b87e6e8d441e8719d75521393
Alex <alex.varna.91@googlemail.com>
2025-01-27 18:30:55 +0100

    Update regarding spring security

--- src/main/java/com/MyTutor2/repo/UserRoleRepository.java
+++ src/main/java/com/MyTutor2/repo/UserRoleRepository.java
@@ -4,7 +4,11 @@ import com.MyTutor2.model.entity.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRoleEntity,Long> {
+
+    Optional<UserRoleEntity> findById(Long id);
 
 }
--- src/main/java/com/MyTutor2/service/impl/UserServiceImpl.java
+++ src/main/java/com/MyTutor2/service/impl/UserServiceImpl.java
@@ -2,32 +2,42 @@ package com.MyTutor2.service.impl;
 
 import com.MyTutor2.model.DTOs.UserRegisterDTO;
 import com.MyTutor2.model.entity.User;
+import com.MyTutor2.model.entity.UserRoleEntity;
 import com.MyTutor2.repo.UserRepository;
+import com.MyTutor2.repo.UserRoleRepository;
 import com.MyTutor2.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
     private ModelMapper modelMapper;
+    private PasswordEncoder passwordEncoder;
+    private UserRoleRepository userRoleRepository;
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder, UserRoleRepository userRoleRepository) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
+        this.userRoleRepository = userRoleRepository;
     }
 
     @Override
     public void registerUser(UserRegisterDTO userRegisterDTO) {
 
         User user = modelMapper.map(userRegisterDTO, User.class);
+        user.setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
 
-
-        //TODO security add encoded password
-
-        //TODO security addrole
-
+        UserRoleEntity userRoleEntity = userRoleRepository.findById(1L).get();
+        List<UserRoleEntity> userRoles = new ArrayList<>();
+        userRoles.add(userRoleEntity);
+        user.setRoles(userRoles);
 
         userRepository.save(user);
 
