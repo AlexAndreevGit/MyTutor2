ad3e7e5e2938cfd11db11a85ef38d0242da1253c
Alex <alex.varna.91@gmail.com>
2025-03-14 21:12:02 +0100

    Update ExRateServiceImpl. Make code more readable.

--- src/main/java/com/MyTutor2/service/impl/ExRateServiceImpl.java
+++ src/main/java/com/MyTutor2/service/impl/ExRateServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -40,7 +41,7 @@ public class ExRateServiceImpl implements ExRateService {
         return exRateRepository.count() > 0;
     }
 
-    //with the restClient we send a http query and we get a JSON response
+
     @Override
     public ExRatesDTO fetchExRates() {
 
@@ -50,7 +51,6 @@ public class ExRateServiceImpl implements ExRateService {
                 .accept(MediaType.APPLICATION_JSON)// we tell to the website "OpenExchangeRates" that we want the response to be in a JSON format
                 .retrieve() // we call the "retrieve()" method . Then step_1. in "educational->REST_Workflow" is happening and we wait for step_2.
                 .body(ExRatesDTO.class); // return ExRatesDTO object. All exchange rate entities are in ExRatesDTO.
-
 
     }
     //Postman -> https://openexchangerates.org/api/latest.json?app_id=e1c2f30d79644bc798c585b49408fbfc&prettyprint=true&show_alternative=false
@@ -65,9 +65,14 @@ public class ExRateServiceImpl implements ExRateService {
             throw new IllegalArgumentException("the exchange rates that should be updated are not based on " + forexApiConfig.getBase() + "but rather on" + exRatesDTO.base());
         }
 
-        //TODO understand and optimise
-        exRatesDTO.rates().forEach((currency, rate) ->
-        {
+
+        Map<String, BigDecimal> rates = exRatesDTO.rates();
+
+        for (Map.Entry<String, BigDecimal> entry : rates.entrySet()) {
+
+            String currency = entry.getKey();
+            BigDecimal rate = entry.getValue();
+
             ExRateEntity exRateEntity = exRateRepository.findByCurrency(currency)   // Example -> we try to find  BGN
                     .orElseGet(() -> {                                              //If it doesn't exist in the DB, then create a new one
                                 ExRateEntity exRateEntityNew = new ExRateEntity();
@@ -76,12 +81,26 @@ public class ExRateServiceImpl implements ExRateService {
                             }
                     );
 
-            //update or set(if new one) the rate
             exRateEntity.setRate(rate);
-
             exRateRepository.save(exRateEntity);
+        }
 
-        });
+//        rates.forEach((currency, rate) ->
+//        {
+//            ExRateEntity exRateEntity = exRateRepository.findByCurrency(currency)   // Example -> we try to find  BGN
+//                    .orElseGet(() -> {                                              //If it doesn't exist in the DB, then create a new one
+//                                ExRateEntity exRateEntityNew = new ExRateEntity();
+//                                exRateEntityNew.setCurrency(currency);
+//                                return exRateEntityNew;
+//                            }
+//                    );
+//
+//            //update or set(if new one) the rate
+//            exRateEntity.setRate(rate);
+//
+//            exRateRepository.save(exRateEntity);
+//
+//        });
 
     }
 
@@ -121,18 +140,10 @@ public class ExRateServiceImpl implements ExRateService {
             toOpt = exRateRepository.findByCurrency(to).map(ExRateEntity::getRate);
         }
 
-//        Optional<BigDecimal> fromOpt = forexApiConfig.getBase().equals(from) ?
-//                Optional.of(BigDecimal.ONE) :
-//                exRateRepository.findByCurrency(from).map(ExRateEntity::getRate);
-//
-//        Optional<BigDecimal> toOpt = forexApiConfig.getBase().equals(to) ?
-//                Optional.of(BigDecimal.ONE) :
-//                exRateRepository.findByCurrency(to).map(ExRateEntity::getRate);
-
         if (fromOpt.isEmpty() || toOpt.isEmpty()) {
             return Optional.empty();
         } else {
-            return Optional.of(toOpt.get().divide(fromOpt.get(), RoundingMode.HALF_DOWN));
+            return Optional.of(toOpt.get().divide(fromOpt.get(),10, RoundingMode.HALF_DOWN)); //10 decimal places while applying half down
         }
     }
 
